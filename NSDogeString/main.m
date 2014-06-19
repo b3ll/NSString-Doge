74752db052995b4abe3cea663e038bd7e5b5d724
Adam Bell <b3ll@fb.com>
2014-06-18 23:24:31 -0700

    Initial Commit

---
+++ NSDogeString/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSDogeString
+//
+//  Created by Adam Bell on 6/18/14.
+//  Copyright (c) 2014 Adam Bell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
